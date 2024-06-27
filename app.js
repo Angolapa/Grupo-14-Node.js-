2608fa35fd40a49487fe03c9e253333050a6f661
Angolapa <josealfredoangola@gmail.com>
2024-06-27 13:45:45 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const config = require('./config');
+
+const clientes = require('./modulos/clientes/rutas')
+
+const app = express();
+
+//Configuracion.
+app.set('port', config.app.port) 
+
+//Rutas.
+app.use('/api/clientes', clientes)
+module.exports = app;
