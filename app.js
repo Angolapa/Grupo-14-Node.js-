73017030ba08ecd509853d578c23cdc47e3398b4
Angolapa <josealfredoangola@gmail.com>
2024-06-30 12:59:01 -0300

    Actualizacion Conexión a la base de datos MySQL

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const morgan = require ('morgan');
 const config = require('./config');
 
-const clientes = require('./src/modulos/clientes/rutas')
+const clientes = require('./src/modulos/clientes/rutas');
+const error  = require('./src/red/errors');
 
 const app = express();
 
+//Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 //Configuracion.
 app.set('port', config.app.port) 
 
 //Rutas.
 app.use('/api/clientes', clientes)
+app.use(error);
+
 module.exports = app;
