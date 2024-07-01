2fbe8f0595c69e7903741e90b9396ad4edb39395
Angolapa <josealfredoangola@gmail.com>
2024-07-01 10:24:32 -0300

    Autenticación, Login, Bcrypt, Json Web Token

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 const express = require('express');
-const morgan = require ('morgan');
+const morgan = require('morgan');
 const config = require('./config');
 
 const clientes = require('./src/modulos/clientes/rutas');
 const usuarios = require('./src/modulos/usuarios/rutas');
-const error  = require('./src/red/errors');
+const auth = require('./src/modulos/auth/rutas');
+const error = require('./src/red/errors');
 
 const app = express();
 
-//Middleware
+
+// Middleware
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-//Configuracion.
-app.set('port', config.app.port) 
+app.use(express.urlencoded({ extended: true }));
 
-//Rutas.
+// Configuración
+app.set('port', config.app.port);
+
+// Rutas
 app.use('/api/clientes', clientes);
 app.use('/api/usuarios', usuarios);
+app.use('/api/auth', auth);
 app.use(error);
 
 module.exports = app;
--- src/modulos/usuarios/controlador.js
+++ src/modulos/usuarios/controlador.js
@@ -1,4 +1,5 @@
 const TABLA = 'usuarios';
+const auth = require('../auth');
 module.exports = function (dbInyectada) {
 
     let db = dbInyectada;
@@ -10,12 +11,38 @@ module.exports = function (dbInyectada) {
     function todos(){
         return db.todos(TABLA);
     }
+
     function uno(id){
         return db.uno(TABLA, id);
     }
-    function agregar(body){
-        return db.agregar(TABLA, body);
+
+   async function agregar(body){
+        const  usuario = {
+            id: body.id,
+            nombre:  body.nombre,
+            activo: body.activo
+        }
+        const  respuesta = await db.agregar(TABLA, usuario);
+        console.log('respuesta',respuesta)
+        var insertId = 0;
+        if(body.id == 0){
+            insertId = respuesta.insertId;
+        }else{
+            insertId = body.id;
+        }
+
+        var respuesta2 = '';
+
+        if(body.usuario || body.password){
+            respuesta2  =  await auth.agregar({
+                id: insertId,
+                usuario: body.usuario,
+                password: body.password
+            })
+        }
+        return  respuesta2;
     }
+
     function eliminar (body){
         return db.eliminar(TABLA, body);
     }
